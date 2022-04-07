768c31730dc02917c064a93159bfdc79336fa651
quillasp <saralfddin93@yahoo.fr>
2022-04-07 15:48:27 +0200

    step 2 ProductService.scala

--- Labo02/src/main/scala/Data/ProductService.scala
+++ Labo02/src/main/scala/Data/ProductService.scala
@@ -9,6 +9,23 @@ trait ProductService:
 
 class ProductImpl extends ProductService:
   // TODO - Part 2 Step 2
-  def getPrice(product: ProductName, brand: String): Double = ???
-  def getDefaultBrand(product: ProductName): BrandName = ???
+  def getPrice(product: ProductName, brand: String): Double = product match
+    case "biere" =>
+      brand match
+        case "farmer" => 1.0
+        case "wittekop" => 2.0
+        case "punkipa" => 3.0
+        case "jackhammer" => 3.0
+        case "tenebreuse" => 4.0
+        case _ => 1.0
+    case "croissant" =>
+      brand match
+        case "cailler" => 2.0
+        case _ => 2.0
+
+
+
+  def getDefaultBrand(product: ProductName): BrandName = product match
+    case "biere" => "boxer"
+    case "croissant" => "maison"
 end ProductImpl
